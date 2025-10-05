979ed97315c761f0c40473a7f269fe2909455b40
get-D <divyatwa5@gmail.com>
2025-10-05 22:24:36 +0530

    combination formula using recursion

---
+++ Recursion/nCr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int nCr(int n, int r){
+    if(r==0 || n==r){
+        return 1;
+    }
+    return nCr(n-1, r-1) + nCr(n-1, r);
+}
+
+int main(){
+    printf("%d \n", nCr(4,2));
+    return 0;
+}
